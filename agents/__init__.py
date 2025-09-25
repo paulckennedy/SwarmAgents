22facf36354270cdcc4dfd446a460048f441a027
Kennedy <Paul.C.Kennedy@p66.com>
2025-09-25 10:39:06 -0500

    Use Jinja2 for PromptStore; add strict-mode tests and env-var tests

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,6 @@
 # Agents package for SwarmAgents
 from .youtube_researcher import YouTubeResearcher
+from .prompts import PromptStore, ps
+from .agent_base import AgentBase
 
-__all__ = ["YouTubeResearcher"]
+__all__ = ["YouTubeResearcher", "PromptStore", "ps", "AgentBase"]
